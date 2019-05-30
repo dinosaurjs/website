0a5ec76db501aaabcd219cd9f585054befb195d3
Mike Pack <mikepackdev@gmail.com>
2019-05-30 17:09:00 -0600

    Wire up schedule.json with schedule.js

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,25 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import { StaticQuery, graphql } from "gatsby"
+
 import './schedule.css'
 
-class Schedule extends Component {
-  constructor() {
-    super()
-}
+const Event = ({ time, title, speaker, description, twitter }) => (
+  <article className="event">
+    <p className="schedule-text time">{time}</p>
+    <p className="schedule-text title">{title}</p>
+    <p className="schedule-text speaker">{speaker}</p>
+    <p className="schedule-text description">{description}</p>
+    <p className="schedule-text twitter">{twitter}</p>
+  </article>
+)
 
-  render() {
-    return (
-      <div className="schedule-panel panel" id="schedule">
-        <header className="panel-header">
-          <h2>Schedule</h2>
-        </header>
-        <p className="schedule-text time">{this.props.schedule.time}</p>
-        <p className="schedule-text title">{this.props.schedule.speaker}</p>
-        <p className="schedule-text description">{this.props.schedule.description}</p>
-        <p className="schedule-text twitter">{this.props.schedule.twitter}</p>
-      </div>
-    )
-  }
-}
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query ScheduleQuery {
+        allScheduleJson {
+          edges {
+            node {
+              time
+              title
+              speaker
+              description
+              twitter
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const events = data.allScheduleJson.edges.map(({ node }) => node);
 
-
-export default Schedule;
+      return (
+        <section className="schedule-panel panel" id="schedule">
+          <header className="panel-header">
+            <h2>Schedule</h2>
+          </header>
+          {events.map(event => <Event {...event} key={event.time} />)}
+        </section>
+      )
+    }}
+  />
+)
